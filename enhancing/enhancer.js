7cf2958029bc4d2f70dcbdb1022ff00dc993a6b8
Aruna Benjamin <arunabenji29@gmail.com>
2019-06-17 14:41:15 -0600

    tested for repair item

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,7 +6,8 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  
+  return {...item}
 }
 
 function fail(item) {
@@ -14,7 +15,8 @@ function fail(item) {
 }
 
 function repair(item) {
-  return { ...item };
+  console.log('item durability ',item.durability)
+  return { ...item,durability:100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,14 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+describe('enhancer.js', () => {
+    describe('repair()', () => {
+        it('check if the durability is 100', () => {
+            console.log('inside the repair test',enhancer.repair({durability:89}).durability)
+            expect(enhancer.repair({durability:89}).durability).toBe(100);
+            expect(enhancer.repair({durability:-89}).durability).toBe(100);
+            expect(enhancer.repair({durability:100}).durability).toBe(100);
+            expect(enhancer.repair({durability:0}).durability).toBe(100);
+        })
+    })
+})
